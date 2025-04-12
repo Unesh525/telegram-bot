f246db038262a411e77c161f99704d444e21a740
Umesh Kumar Meghwal <umeshmeghwa605@gmail.com>
2025-04-12 17:33:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "7333881556:AAHjTujLua8VPJkVWHnbiFuWN40O07V94is"
+# अब TOKEN env variable से लिया जाएगा (Railway या local .env file से)
+TOKEN = os.environ["TOKEN"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hey Umesh! Bot is live ✅")
